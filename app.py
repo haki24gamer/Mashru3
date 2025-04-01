5a5e69698602f9abfd971dded8bd1f1ed6531bde
Haki <36126379+haki24gamer@users.noreply.github.com>
2025-04-01 10:48:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1470,4 +1470,5 @@ def update_specification(specification_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port, host="0.0.0.0")
